20b03c6cd6ceb6b1fba07efb0b8d8622781cb309
krsnananda <krsnananda@outlook.com>
2019-12-03 22:45:50 -0300

    - Inicia tela de formulário

--- src/Restaurants.js
+++ src/Restaurants.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Restaurants.css';
-
+import logo from './share-eat.svg';
+import './Places.css';
 export default class Restaurantes extends React.Component {
-
-    render() {
-        return (
-            <div className="formulario-background">
-                <h2>{'Welcome to About !'}</h2>
-            </div>
-        );
-    }
+   render() {
+      return (
+         <div className="formulario-background">
+               <header>
+                  <img src={logo} className="logo" alt="logo" />
+               </header>
+                  <h1> Restaurante Selecionado </h1>
+         </div>
+      );
+   }
 }
