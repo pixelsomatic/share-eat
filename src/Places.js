207fda79ae5e6e45958ab32ca625a4d4cc57eb6c
krsnananda <krsnananda@outlook.com>
2019-12-05 02:15:56 -0300

    - Exibe nome de restaurantes através de requisição ao arquivo JSON

--- src/Places.js
+++ src/Places.js
@@ -4,41 +4,57 @@ import logo from './share-eat.svg';
 import './Places.css';
 import addWhite from './ic_add_white.svg';
 import {Link} from "react-router-dom";
-import teste from './Teste';
-
+import testes from './teste.json';
 
 export default class Places extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      testes: testes
     };
   }  
   
-  // CRUD em React: https://medium.com/@dnvtrn/como-fazer-um-crud-com-create-react-app-f0402ff89c05
-  // Cannot read map of undefined: https://stackoverflow.com/questions/24706267/cannot-read-property-map-of-undefined
-  // Manipulando JSON: https://www.golangprograms.com/display-json-data-in-reactjs.html
-  
-  
-  // const nameRestaurant = teste.menuItems;
-  // {/* <ul> */}
-  //   {nameRestaurant.map(n => (<li>{n}</li>))}
-  // {/* </ul> */}
-  
   render () {
-      return (
-        <div className="Places">
+    // Busca nome de restaurantes via JSON
+    const lista = this.state.testes.map((testes) => <p className="show-restaurant" key={testes}>{`${testes.name}`}</p>);
+    console.log(testes[0]);
+
+    return( 
+      <div className="Places">
           <header className="Places-header">
             <img src={logo} className="logo" alt="logo" />
             <h1> Lugares </h1>
             <h6> lugares cadastrados </h6>
             <img src={background} className="App-background" alt="background" />
-                <div className="card-restaurant">
+              
+              <div className="card-restaurant">
                   <Link to="/detalhes" className="links">
-                      <div className="restaurant-name py-2 px-4"> restaurantes </div> 
+                      <div className="restaurant-name py-2 px-4"> {lista[0]} </div> 
                       <div className="dishes"> pratos </div>
                   </Link>
-                </div>
+              </div>
+
+              <div className="card-restaurant">
+                  <Link to="/detalhes" className="links">
+                      <div className="restaurant-name py-2 px-4"> {lista[1]} </div> 
+                      <div className="dishes"> pratos </div>
+                  </Link>
+              </div>
+
+              <div className="card-restaurant">
+                <Link to="/detalhes" className="links">
+                    <div className="restaurant-name py-2 px-4"> {lista[2]} </div> 
+                    <div className="dishes"> pratos </div>
+                </Link>
+              </div>
+
+              <div className="card-restaurant">
+                  <Link to="/detalhes" className="links">
+                      <div className="restaurant-name py-2 px-4"> {lista[3]} </div> 
+                      <div className="dishes"> pratos </div>
+                  </Link>
+              </div>
+
               <Link to="/restaurantes">
                 <button className="add-menu">
                   <img src={addWhite}  alt="add"/>
@@ -50,4 +66,6 @@ export default class Places extends React.Component {
     }
   }
   
-  
+  // CRUD em React: https://medium.com/@dnvtrn/como-fazer-um-crud-com-create-react-app-f0402ff89c05
+  // Cannot read map of undefined: https://stackoverflow.com/questions/24706267/cannot-read-property-map-of-undefined
+  // Manipulando JSON: https://www.golangprograms.com/display-json-data-in-reactjs.html
