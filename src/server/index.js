7eff49354552606cdb127f5d20480700502fd30e
krsnananda <krsnananda@outlook.com>
2019-12-16 00:09:27 -0300

    - Adiciona socket.io para fazer deploy no heroku

---
+++ src/server/index.js
@@ -0,0 +1,16 @@
+const path = require(' path ');
+const express = require (' express ');
+const app = express ();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('/', (req, res, next) => 
+    res.sendFile(__dirname + './index.html'));
+
+    //sockets teste
+io.on('connection', socket => 
+    socket.emit('hello', {message: 'hello from server!' }));
+
+    server.listen(port);
