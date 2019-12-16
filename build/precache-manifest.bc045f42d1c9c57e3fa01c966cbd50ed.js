3c22beba20b9e36a491d5f13652103176ed24732
krsnananda <krsnananda@outlook.com>
2019-12-16 00:16:07 -0300

    Heroku deploy

---
+++ build/precache-manifest.bc045f42d1c9c57e3fa01c966cbd50ed.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892582321fc099d58db4d2de82fa2bc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2074fe64bb7dc1ff054e",
+    "url": "/static/css/main.ee76e41b.chunk.css"
+  },
+  {
+    "revision": "55aa80eca84a1f2da2a3",
+    "url": "/static/js/2.2d65ad3b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.2d65ad3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2074fe64bb7dc1ff054e",
+    "url": "/static/js/main.c1613ca6.chunk.js"
+  },
+  {
+    "revision": "493952dcd76eb2fcf500",
+    "url": "/static/js/runtime-main.7cb9b0c7.js"
+  },
+  {
+    "revision": "7b7ead0799465f1b0c96f89c340b1dd6",
+    "url": "/static/media/back-button.7b7ead07.png"
+  },
+  {
+    "revision": "5265d181453899bb7b27d7ef306002d7",
+    "url": "/static/media/background-paint.5265d181.svg"
+  },
+  {
+    "revision": "7d4292e81eaad79de3f17f877574db77",
+    "url": "/static/media/ic_add_black.7d4292e8.svg"
+  },
+  {
+    "revision": "9f774697d8a6183466591872148a162b",
+    "url": "/static/media/ic_add_white.9f774697.svg"
+  },
+  {
+    "revision": "997dc5dc56778d3a101b8f2d876b2cd1",
+    "url": "/static/media/share-eat.997dc5dc.svg"
+  }
+]);
